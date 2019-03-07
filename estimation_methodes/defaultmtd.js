92c71c5e8260d694bc3e14b1bd15182cc666ce3a
ekiriano <ekiriano34@gmail.com>
2019-03-07 14:42:45 +0100

    mtd inverstissment forcast

--- estimation_methodes/defaultmtd.js
+++ estimation_methodes/defaultmtd.js
@@ -4,29 +4,32 @@ module.exports = {
 
     // SCRAPPER LE MAX POSSIBLE DE BIENS QUI ONT LES MEME CARACTERISTIQUES AVEC DEFAULTBIEN ET LES METTRE DANS UN TABLEAU D'OBJETS
     // JAPPELLE LE TABLEAU TAB FOR NOW
-    
-    for(var bien in tab){
+
+    for (var bien in tab) {
       DefaultBien.prix_estimation += bien.prix_estimation;
-      DefaultBien.prix_estimation = DefaultBien.prix_estimation/tab.length();
+      DefaultBien.prix_estimation = DefaultBien.prix_estimation / tab.length();
     }
     return DefaultBien;
   },
-  
-  
-  
+
   defaultEstimationReference: DefaultBien => {
     // SCRAPPER UN BIEN QUI EST LE PLUS SIMILAIRE A DEFAULTBIEN ET LE METTRE DANS UNE VARIABLE QUE JAPPELLE objet FOR NOW
-    if(objet.surface === DefaultBien.surface){
-      Defaultbien.prix_estimation= objet.prix_estimation;
+    if (objet.surface === DefaultBien.surface) {
+      Defaultbien.prix_estimation = objet.prix_estimation;
       return DefaultBien;
     }
-    
-    var a=objet.prix_estimation;
-    var b=objet.surface;
-    var c=DefaultBien.surface;
-    DefaultBien.prix_estimation=(b*c)/a;
+
+    var a = objet.prix_estimation;
+    var b = objet.surface;
+    var c = DefaultBien.surface;
+    DefaultBien.prix_estimation = (b * c) / a;
+    return DefaultBien;
+  },
+
+  defaultEstimationInvestisment: (DefaultBien, NombreAnnee) => {
+    const avg_prix = DefaultBien.getPrixBienSimilaire();
+    const growth_percentage = getAvgGrowth();
+    //DefaultBien.prix_estimation = compounded interest on nombre annee
     return DefaultBien;
   }
-  
-  
 };
