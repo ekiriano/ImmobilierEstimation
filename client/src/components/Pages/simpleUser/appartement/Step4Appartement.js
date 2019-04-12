31b64f07a47c6e18593ccb41d0d889829d1924ca
Mohamed Bouchengour El Bahja <34141631+Mbouchengour@users.noreply.github.com>
2019-04-12 06:22:09 +0200

    Update Step4Appartement.js

--- client/src/components/Pages/simpleUser/appartement/Step4Appartement.js
+++ client/src/components/Pages/simpleUser/appartement/Step4Appartement.js
@@ -1,70 +1,73 @@
 import React, { Component } from "react";
 import classnames from "classnames";
-
 class Step4Appartement extends Component {
-    render() {
-      const errors = this.props.errors;
-      if (this.props.currentStep !== 1) {
-        return null;
-      }
-      return (
-        <div>
-          <h1>Etape 1 - L'adresse</h1>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.rue
-                })}
-                type="text"
-                name="rue"
-                placeholder="rue"
-                value={this.props.rue}
-                onChange={this.props.onChange}
-                required
-              />
-  
-              {errors.rue && <p class="help is-danger">{errors.rue}</p>}
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.code_postal
-                })}
-                type="text"
-                name="code_postal"
-                placeholder="code postal"
-                value={this.props.code_postal}
-                onChange={this.props.onChange}
-                required
-              />
-              {errors.code_postal && (
-                <p class="help is-danger">{errors.code_postal}</p>
-              )}
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.ville
-                })}
-                type="text"
-                name="ville"
-                placeholder="ville"
-                value={this.props.ville}
-                onChange={this.props.onChange}
-                required
-              />
-              {errors.ville && <p class="help is-danger">{errors.ville}</p>}
-            </div>
+  render() {
+    const errors = this.props.errors;
+    if (this.props.currentStep !== 4) {
+      return null;
+    }
+    return (
+      <div>
+        <h1>Etape 4 - Auxiliaire</h1>
+        <div className="field">
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.annee_construction
+              })}
+              type="text"
+              name="annee_construction"
+              placeholder="annee_construction"
+              value={this.props.annee_construction}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.annee_construction && (
+              <p className="help is-danger">{errors.annee_construction}</p>
+            )}
           </div>
         </div>
-      );
-    }
+        <div className="field">
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.diagnostic_performance_energetique
+              })}
+              type="text"
+              name="diagnostic_performance_energetique"
+              placeholder="diagnostic performance energetique "
+              value={this.props.diagnostic_performance_energetique}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.diagnostic_performance_energetique && (
+              <p className="help is-danger">
+                {errors.diagnostic_performance_energetique}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.etat_bien
+              })}
+              type="text"
+              name="etat_bien"
+              placeholder=" etat bien"
+              value={this.props.etat_bien}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.etat_bien && (
+              <p className="help is-danger">{errors.etat_bien}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
   }
-  
-  export default Step4Appartement;
-  
+}
+
+export default Step4Appartement;
