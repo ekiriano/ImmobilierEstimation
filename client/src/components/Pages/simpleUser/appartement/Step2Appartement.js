a5498ccb1b65caa3429a49f9be0ed8d581907e7c
Mohamed Bouchengour El Bahja <34141631+Mbouchengour@users.noreply.github.com>
2019-04-12 06:18:01 +0200

    Update Step2Appartement.js

--- client/src/components/Pages/simpleUser/appartement/Step2Appartement.js
+++ client/src/components/Pages/simpleUser/appartement/Step2Appartement.js
@@ -2,69 +2,84 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Step2Appartement extends Component {
-    render() {
-      const errors = this.props.errors;
-      if (this.props.currentStep !== 1) {
-        return null;
-      }
-      return (
-        <div>
-          <h1>Etape 1 - L'adresse</h1>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.rue
-                })}
-                type="text"
-                name="rue"
-                placeholder="rue"
-                value={this.props.rue}
-                onChange={this.props.onChange}
-                required
-              />
-  
-              {errors.rue && <p class="help is-danger">{errors.rue}</p>}
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.code_postal
-                })}
-                type="text"
-                name="code_postal"
-                placeholder="code postal"
-                value={this.props.code_postal}
-                onChange={this.props.onChange}
-                required
-              />
-              {errors.code_postal && (
-                <p class="help is-danger">{errors.code_postal}</p>
-              )}
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.ville
-                })}
-                type="text"
-                name="ville"
-                placeholder="ville"
-                value={this.props.ville}
-                onChange={this.props.onChange}
-                required
-              />
-              {errors.ville && <p class="help is-danger">{errors.ville}</p>}
-            </div>
+  render() {
+    const errors = this.props.errors;
+    if (this.props.currentStep !== 2) {
+      return null;
+    }
+    return (
+      <div>
+        <h1>Etape 2 - Surface</h1>
+        <div className="field">
+          <div className="control">
+            <label>
+              {" "}
+              Surface habitable : <br />
+            </label>
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.surface_habitable
+              })}
+              type="text"
+              name="surface_habitable"
+              placeholder="surface habitable"
+              value={this.props.surface_habitable}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.surface_habitable && (
+              <p class="help is-danger">{errors.surface_habitable}</p>
+            )}
           </div>
         </div>
-      );
-    }
+        <div className="field">
+          <label>
+            {" "}
+            Surface habitable terrain : <br />
+          </label>
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.surface_totale_terrain
+              })}
+              type="text"
+              name="surface_totale_terrain"
+              placeholder="surface totale terrain"
+              value={this.props.surface_totale_terrain}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.surface_totale_terrain && (
+              <p class="help is-danger">{errors.surface_totale_terrain}</p>
+            )}
+          </div>
+        </div>
+        <div className="field">
+          <label>
+            Surface habitable constructible : <br />
+          </label>
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.surface_habitable_constructible
+              })}
+              type="text"
+              name="surface_habitable_constructible"
+              placeholder="surface habitable constructible"
+              value={this.props.surface_habitable_constructible}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.surface_habitable_constructible && (
+              <p class="help is-danger">
+                {errors.surface_habitable_constructible}
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
   }
-  
-  export default Step2Appartement;
-  
+}
+
+export default Step2Appartement;
