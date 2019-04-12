77f1b0bf02cc88be8a54a3bc0b93b8a1f455fe63
ekiriano <ekiriano34@gmail.com>
2019-04-12 11:03:56 +0200

    test 2 deploy config

--- server.js
+++ server.js
@@ -36,14 +36,12 @@ app.use("/api/users", users);
 app.use("/api/estimation/default", defaultEstimation);
 
 // For heroku deploy
-
-if(process.env.NODE_ENV == 'production'){
+if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get("*",(req,res)=>{
-    res.sendfile(path.resolve(__dirname,'client','build','index.html'));
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
 }
-
 
 const port = process.env.PORT || 5000;
 
