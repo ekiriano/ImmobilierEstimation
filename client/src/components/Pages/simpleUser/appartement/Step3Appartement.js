5651ac89336010000d6d7494c2227647dba41e36
Mohamed Bouchengour El Bahja <34141631+Mbouchengour@users.noreply.github.com>
2019-04-12 06:20:23 +0200

    Update Step3Appartement.js

--- client/src/components/Pages/simpleUser/appartement/Step3Appartement.js
+++ client/src/components/Pages/simpleUser/appartement/Step3Appartement.js
@@ -2,69 +2,73 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Step3Appartement extends Component {
-    render() {
-      const errors = this.props.errors;
-      if (this.props.currentStep !== 1) {
-        return null;
-      }
-      return (
-        <div>
-          <h1>Etape 1 - L'adresse</h1>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.rue
-                })}
-                type="text"
-                name="rue"
-                placeholder="rue"
-                value={this.props.rue}
-                onChange={this.props.onChange}
-                required
-              />
-  
-              {errors.rue && <p class="help is-danger">{errors.rue}</p>}
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.code_postal
-                })}
-                type="text"
-                name="code_postal"
-                placeholder="code postal"
-                value={this.props.code_postal}
-                onChange={this.props.onChange}
-                required
-              />
-              {errors.code_postal && (
-                <p class="help is-danger">{errors.code_postal}</p>
-              )}
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                className={classnames("input is-medium", {
-                  "is-danger": errors.ville
-                })}
-                type="text"
-                name="ville"
-                placeholder="ville"
-                value={this.props.ville}
-                onChange={this.props.onChange}
-                required
-              />
-              {errors.ville && <p class="help is-danger">{errors.ville}</p>}
-            </div>
+  render() {
+    const errors = this.props.errors;
+    if (this.props.currentStep !== 3) {
+      return null;
+    }
+    return (
+      <div>
+        <h1>Etape 3 - Pieces</h1>
+
+        <div className="field">
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.nombre_pieces
+              })}
+              type="text"
+              name="nombre_pieces"
+              placeholder="nombre piece"
+              value={this.props.nombre_pieces}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.nombre_pieces && (
+              <p class="help is-danger">{errors.nombre_pieces}</p>
+            )}
           </div>
         </div>
-      );
-    }
+        <div className="field">
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.nombre_salle_bain
+              })}
+              type="text"
+              name="nombre_salle_bain"
+              placeholder="nombre salle bain"
+              value={this.props.nombre_salle_bain}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.nombre_salle_bain && (
+              <p class="help is-danger">{errors.nombre_salle_bain}</p>
+            )}
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <input
+              className={classnames("input is-medium", {
+                "is-danger": errors.nombre_niveaux
+              })}
+              type="text"
+              name="nombre_niveaux"
+              placeholder="nombre niveaux"
+              value={this.props.nombre_niveaux}
+              onChange={this.props.onChange}
+              required
+            />
+            {errors.nombre_niveaux && (
+              <p class="help is-danger">{errors.nombre_niveaux}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
   }
-  
-  export default Step3Appartement;
-  
+}
+
+export default Step3Appartement;
+
