c99f7d87b609623a797200076a06257f78479039
ekiriano <ekiriano34@gmail.com>
2019-04-12 10:53:03 +0200

    changes for production tests

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+//path module
+const path = require('path');
 
 // Api Routes Importation
 const users = require("./routes/api/users");
@@ -32,6 +34,16 @@ require("./config/passport")(passport);
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/estimation/default", defaultEstimation);
+
+// For heroku deploy
+
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('client/build'));
+  app.get("*",(req,res)=>{
+    res.sendfile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
+
 
 const port = process.env.PORT || 5000;
 
