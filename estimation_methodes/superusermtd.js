3ba5d6ba0279df12675e26497b77d92306809ea0
Mohamed Bouchengour El Bahja <34141631+Mbouchengour@users.noreply.github.com>
2019-04-21 02:54:54 +0200

    Update superusermtd.js

--- estimation_methodes/superusermtd.js
+++ estimation_methodes/superusermtd.js
@@ -1,76 +1,118 @@
 module.exports = {
-  HouseEstimationComparaison: Bien => {
+  EstimationComparaison: Bien => {
 
-  	var json = require('./fakedatamaison.json');
+  	var json = require('./data.json');
 
-  	var i = 1;
-  	for(var bien=0;bien<json.maisons.length;bien++){
-  		if((Bien.estimation-10000 <= json.maisons[bien].estimation)  && (json.maisons[bien].estimation<= Bien.estimation+10000)){
-  		      Bien.estimation += json.maisons[bien].estimation;
-  		      i++;
-  		  }
-  	}
+    var nbr=0;
+    var estime=0;
+    for(var bien=0;bien<json.maisons.length;bien++){
+      if(json.maisons[bien].bienImmobillier.localisationBien.codePostal== Bien.bienImmobillier.localisationBien.codePostal){
+      estime=estime+json.maisons[bien].estimation.prix_estimation.methodecomparaison;
+      nbr++;
+    }
+  }
 
-  	Bien.estimation /= i;
+    estime=estime/nbr;
 
-    console.log(Bien.estimation);
+    Bien.estimation.prix_estimation.methodecomparaison=estime;
+    Bien.estimation.prix_estimation.methodecomparaison=Bien.estimation.prix_estimation.methodecomparaison-(Bien.estimation.decoteBienOccupe+Bien.estimation.autresElements+Bien.estimation.travauxARealiser+Bien.estimation.valorisationTerrain+Bien.estimation.renove);
+
     return Bien;
   },
 
-  HouseEstimationReference: Bien => {
-  	var json = require('./fakedatamaison.json');
-  	var objet;
-  	for(var bien=0;bien<json.maisons.length;bien++){
-  		if((Bien.estimation-100000 <= json.maisons[bien].estimation)  && (json.maisons[bien].estimation<= Bien.estimation+100000)){
-  			objet=json.maisons[bien];
-  		  }
-  	}
+  EstimationReference: Bien => {
+  	var json = require('./data.json');
 
+    var bienSimilaire;
 
-      if (objet.bienImmobillier.informations.surfaceHabitable == Bien.bienImmobillier.informations.surfaceHabitable) {
-      Bien.estimation = objet.estimation;
-      return Bien;
+    for(var bien=0;bien<json.maisons.length;bien++){
+      if(json.maisons[bien].informations.surfaceHabitable-5<=Bien.informations.surfaceHabitable || Bien.informations.surfaceHabitable <= json.maisons[bien].informations.surfaceHabitable+5){
+        bienSimilaire=json.maisons[bien];
+      }
     }
 
-    var a = objet.estimation;
-    var b = objet.bienImmobillier.informations.surfaceHabitable;
-    var c = Bien.bienImmobillier.informations.surfaceHabitable;
-    Bien.estimation = ((a * c) / b);
-        console.log(Bien.estimation);
+      var a= bienSimilaire.informations.surfaceHabitable;
+      var b= Bien.informations.surfaceHabitable;
+      var c = bienSimilaire.estimation.prix_estimation.methodereference;
+      Bien.estimation.prix_estimation.methodereference= (b*c)/a;
 
-    return Bien;
+    if(bienSimilaire.informations.nombrePieces > Bien.informations.nombrePieces ){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference + (1000*(bienSimilaire.informations.nombrePieces-Bien.informations.nombrePieces));
+    }
+    if(Bien.informations.nombrePieces > bienSimilaire.informations.nombrePieces ){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference - (1000*(Bien.informations.nombrePieces-bienSimilaire.informations.nombrePieces));
+    }
+    ////////////
+    if(bienSimilaire.informations.nombreChambres > Bien.informations.nombreChambres){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference + (1000*(bienSimilaire.informations.nombreChambres-Bien.informations.nombreChambres));
+    }
+    if(Bien.informations.nombreChambres > bienSimilaire.informations.nombreChambres){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference - (1000*(Bien.informations.nombreChambres-bienSimilaire.informations.nombreChambres));
+    }
+    ///////////
+    if(bienSimilaire.informations.nombreNiveaux > Bien.informations.nombreNiveaux){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference + (1000*(bienSimilaire.informations.nombreNiveaux-Bien.informations.nombreNiveaux));
+    }
+    if(Bien.informations.nombreNiveaux > bienSimilaire.informations.nombreNiveaux){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference - (1000*(Bien.informations.nombreNiveaux-bienSimilaire.informations.nombreNiveaux));
+    }
+    //////////
+    if(bienSimilaire.informations.prixm2 > Bien.informations.prixm2){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference + (1000*(bienSimilaire.informations.prixm2-Bien.informations.prixm2));
+    }
+    if(Bien.informations.prixm2 > bienSimilaire.informations.prixm2){
+      Bien.estimation.prix_estimation.methodereference= Bien.estimation.prix_estimation.methodereference - (1000*(Bien.informations.prixm2-bienSimilaire.informations.prixm2));
+    }
+
+    Bien.estimation.prix_estimation.methodereference=Bien.estimation.prix_estimation.methodereference-(Bien.estimation.decoteBienOccupe+Bien.estimation.autresElements+Bien.estimation.travauxARealiser+Bien.estimation.valorisationTerrain+Bien.estimation.renove);
   },
 
- /* defaultEstimationInvestisment: (DefaultBien, NombreAnnee) => {
-    const avg_prix = DefaultBien.getPrixBienSimilaire();
-    const growth_percentage = getAvgGrowth();
-    //DefaultBien.prix_estimation = compounded interest on nombre annee
-    return DefaultBien;
-  }*/
-
-  // ---------------- waiting on abed to update appartement model
-
-  AppartementEstimationComparaison: Bien => {},
-  AppartementEstimationReference: Bien => {},
+  EstimationCapitalisation: Bien => {
+    Bien.estimation.prix_estimation.methodecapitalisation= (Bien.informations.valeurLocative * 12)/ (Bien.informations.tauxCapitalisation/100);
+    Bien.estimation.prix_estimation.methodecapitalisation=Bien.estimation.prix_estimation.methodecapitalisation-(Bien.estimation.decoteBienOccupe+Bien.estimation.autresElements+Bien.estimation.travauxARealiser+Bien.estimation.valorisationTerrain+Bien.estimation.renove);
+  },
 
 };
 
-var test = {
-	user: 0,
-	client: {civilite: "Arlington Road",
-		prenom: "",
-		nom: "" ,
-		email: "cory.connor@dynaroc.name",
-		telephone: 195 },
-	bienImmobillier: {
-		informations: {surfaceHabitable: 25,
-			prixm2: 10243 }
-		},
-    estimation: 300000
+/*var test =     {
+  "bienImmobillier": {
+      "localisationBien": {
+      "codePostal": 34000 }
+      },
+  
 
-    };
+  "informations": {
+      "surfaceHabitable": 200,
+      "nombrePieces":5,
+      "nombreChambres":1,
+      "nombreNiveaux":1,
+      "prixm2":21,
+      "valeurLocative": 1200,
+      "tauxCapitalisation": 4.206778355678784
+      },
+
+
+  "estimation": {
+    "decoteBienOccupe": 2920,
+
+      "autresElements": 9784,
+
+      "travauxARealiser": 4376,
+
+      "valorisationTerrain": 9315,
+
+      "renove": 6740,
+
+      "prix_estimation": {
+        "methodecomparaison": 0,
+        "methodereference": 0,
+        "methodecapitalisation":0
+          }
+      }
+    };*/
 
 
 
-module.exports.HouseEstimationComparaison(test);
-module.exports.HouseEstimationReference(test);
+/*module.exports.EstimationComparaison();
+module.exports.EstimationReference();
+module.exports.EstimationCapitalisation();*/
