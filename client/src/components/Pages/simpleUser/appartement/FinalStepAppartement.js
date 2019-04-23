e695aa4f3d487acaeffe4a2c767e5cd136330e12
Mohamed Bouchengour El Bahja <34141631+Mbouchengour@users.noreply.github.com>
2019-04-23 06:17:49 +0200

    Update FinalStepAppartement.js

--- client/src/components/Pages/simpleUser/appartement/FinalStepAppartement.js
+++ client/src/components/Pages/simpleUser/appartement/FinalStepAppartement.js
@@ -1,4 +1,5 @@
-
+import axios from 'axios';
+import {saveAs} from 'file-saver';
 
 import React, { Component } from 'react'
 
@@ -11,10 +12,22 @@ import React, { Component } from 'react'
       return null;
     }
 
+    createPDF = {} => {
+      axios.post('/createPDF', this.props)
+      .then(() => axios.get('fetch-pdf', {responseType: 'blob'}))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], {type: 'application/pdf'})
+
+        saveAs(pdfBlob, 'newpdf.pdf');
+      })
+    }
+
     return (
       <div>
         <div>
             <h1> Votre bien a été estimmé à : {this.props.prix_estimation}  € </h1> 
+
+            <button onClick={this.createPDF}> Download PDF</button>
         </div>
       </div>
     )
