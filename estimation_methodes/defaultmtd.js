14632ff147f2ec1ef00d9d13cff1b309af02ed1f
Mohamed Bouchengour El Bahja <34141631+Mbouchengour@users.noreply.github.com>
2019-04-24 21:52:20 +0200

    Update defaultmtd.js

--- estimation_methodes/defaultmtd.js
+++ estimation_methodes/defaultmtd.js
@@ -3,60 +3,55 @@ module.exports = {
   defaultEstimationAppartment : DefaultAppartment => {
     var json = require('../datasets/AppartementData.json');
 
-
     var nbr=0;
     var estime=0;
     var surfacemoy=0;
+    var estimmation=0;
+
     for(var bien=0;bien<json.maisons.length;bien++){
-      if(json.maisons[bien].codePostal== DefaultAppartment.codePostal){
+      if(json.maisons[bien].code_postal== DefaultAppartment.code_postal){
       estime=estime+json.maisons[bien].prix_estimation;
       surfacemoy=surfacemoy + json.maisons[bien].surface;
       nbr++;
     }
   }
+
   surfacemoy=surfacemoy/nbr;
-
-
   estime=estime/nbr;
 
   var a= surfacemoy;
   var b= DefaultAppartment.surface;
   var c = estime;
-  var estimmation = Math.round( (b*c)/a) ;
+  estimmation = Math.round( (b*c)/a) ;
   DefaultAppartment.prix_estimation = estimmation;
-  console.log(DefaultAppartment.prix_estimation);
-
   return DefaultAppartment;
 
   },
 
   defaultEstimationHouse: DefaultHouse => {
-
     var json = require('../datasets/HouseData.json');
-
 
     var nbr=0;
     var estime=0;
     var surfacemoy=0;
+    var estimmation=0;
+
     for(var bien=0;bien<json.maisons.length;bien++){
-      if(json.maisons[bien].codePostal== DefaultHouse.codePostal){
+      if(json.maisons[bien].code_postal== DefaultHouse.code_postal){
       estime=estime+json.maisons[bien].prix_estimation;
       surfacemoy=surfacemoy + json.maisons[bien].surface_habitable;
       nbr++;
     }
   }
+
   surfacemoy=surfacemoy/nbr;
-
-
   estime=estime/nbr;
 
   var a= surfacemoy;
   var b= DefaultHouse.surface_habitable;
   var c = estime;
-  var estimmation = Math.round( (b*c)/a) ;
+  estimmation = Math.round( (b*c)/a) ;
   DefaultHouse.prix_estimation = estimmation;
-  console.log(DefaultHouse.prix_estimation);
-
   return DefaultHouse;
 
   },
