380c225e4019b9c197712dc5558bf813ef1d4604
ekiriano <ekiriano34@gmail.com>
2019-04-24 20:40:24 +0200

    snackbars for super user actions

--- client/src/actions/defaultAppartementActions.js
+++ client/src/actions/defaultAppartementActions.js
@@ -1,91 +1,92 @@
 import axios from "axios";
-import {GET_ERRORS,GET_SIMPLE_SAVED_APPARTEMENTS, NEW_SIMPLE_ESTIMATION_APPARTEMENT, DELETE_SIMPLE_SAVED_APPARTEMENT} from "./types";
-import {toast} from 'react-toastify';
+import {
+  GET_ERRORS,
+  GET_SIMPLE_SAVED_APPARTEMENTS,
+  NEW_SIMPLE_ESTIMATION_APPARTEMENT,
+  DELETE_SIMPLE_SAVED_APPARTEMENT
+} from "./types";
+import { toast } from "react-toastify";
 
 export const submitDefaultAppartement = defaultAppartementData => dispatch => {
   axios
     .post("/api/estimation/default/appartement", defaultAppartementData)
-    .then(res => 
-      {
-        dispatch({
-          type : NEW_SIMPLE_ESTIMATION_APPARTEMENT,
-          payload : res.data
-        })
-        toast.success("Votre appartement a été estimmé avec succés");
-      }
-    )
-    .catch(err =>
-      {
-        dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data
-        })
-        toast.error("Erreur lors de l'estimmation : Veuillez corriger les erreurs indiquées !")
-      }
-    );
+    .then(res => {
+      dispatch({
+        type: NEW_SIMPLE_ESTIMATION_APPARTEMENT,
+        payload: res.data
+      });
+      toast.success("Votre appartement a été estimmé avec succés");
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+      toast.error(
+        "Erreur lors de l'estimmation : Veuillez corriger les erreurs indiquées !"
+      );
+    });
 };
 
 export const submitDefaultAppartementSave = defaultAppartementData => dispatch => {
   axios
     .post("/api/estimation/default/appartement/save", defaultAppartementData)
-    .then(res =>
-      {
-        dispatch({
-          type : NEW_SIMPLE_ESTIMATION_APPARTEMENT,
-          payload : res.data
-        })
-        toast.success("Votre appartment a été estimmé et sauvegardé  avec succés");
-      }  
-    )
-    .catch(err =>
-      {
-        dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data
-        })
-        toast.error("Erreur lors de l'estimmation : Veuillez corriger les erreurs indiquées !")
-      }
-    );
+    .then(res => {
+      dispatch({
+        type: NEW_SIMPLE_ESTIMATION_APPARTEMENT,
+        payload: res.data
+      });
+      toast.success(
+        "Votre appartment a été estimmé et sauvegardé  avec succés"
+      );
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+      toast.error(
+        "Erreur lors de l'estimmation : Veuillez corriger les erreurs indiquées !"
+      );
+    });
 };
 
 export const getSavedAppartements = () => dispatch => {
   axios
-  .get("/api/estimation/default/appartements/saved")
-  .then(res =>
-    dispatch({
-      type : GET_SIMPLE_SAVED_APPARTEMENTS,
-      payload : res.data
-    })
-  )
-  .catch(err => 
-    dispatch({
-      type : GET_SIMPLE_SAVED_APPARTEMENTS,
-      payload : null
-    })
-  );
+    .get("/api/estimation/default/appartements/saved")
+    .then(res =>
+      dispatch({
+        type: GET_SIMPLE_SAVED_APPARTEMENTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_SIMPLE_SAVED_APPARTEMENTS,
+        payload: null
+      })
+    );
 };
 
 //deleted saved estimation
 
 export const deleteSavedAppartement = id => dispatch => {
   axios
-  .delete(`/api/estimation/default/appartements/saved/${id}`)
-  .then(res =>
-    {
+    .delete(`/api/estimation/default/appartements/saved/${id}`)
+    .then(res => {
       dispatch({
         type: DELETE_SIMPLE_SAVED_APPARTEMENT,
-        payload : id,
-      })
-      toast.warn("Votre Estimation a été suprimée avec succes ! ")
-    }
-  )
-  .catch(err =>
-    {
+        payload: id
+      });
+      toast.warn("Votre Estimation a été suprimée avec succès ! ");
+    })
+    .catch(err => {
       dispatch({
-        type:GET_ERRORS,
-        payload : err.response.data
-      })
-      toast.error("Une Erreur a eu lieu lors de la suppression de cette estimmation : Veuillez réessayer plus tard");
-    }
-  );
-}
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+      toast.error(
+        "Une Erreur a eu lieu lors de la suppression de cette estimmation : Veuillez réessayer plus tard"
+      );
+    });
+};
--- client/src/actions/superBienActions.js
+++ client/src/actions/superBienActions.js
@@ -1,56 +1,70 @@
 import axios from "axios";
-import {GET_ERRORS,GET_SUPER_SAVED_BIENS, NEW_SUPER_ESTIMATION_BIEN, DELETE_SUPER_SAVED_BIEN} from "./types";
+import {
+  GET_ERRORS,
+  GET_SUPER_SAVED_BIENS,
+  NEW_SUPER_ESTIMATION_BIEN,
+  DELETE_SUPER_SAVED_BIEN
+} from "./types";
 
+import { toast } from "react-toastify";
 
 export const submitSuperBienSave = superBienData => dispatch => {
   axios
     .post("/api/estimation/super/bien/save", superBienData)
-    .then(res =>
+    .then(res => {
       dispatch({
-        type : NEW_SUPER_ESTIMATION_BIEN,
-        payload : res.data
-      })
-    )
-    .catch(err =>
+        type: NEW_SUPER_ESTIMATION_BIEN,
+        payload: res.data
+      });
+      toast.success("Votre bien a été estimmé et sauvegardé avec succés");
+    })
+    .catch(err => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      })
-    );
+      });
+      toast.error(
+        "Erreur lors de l'estimmation : Veuillez corriger les erreurs indiquées !"
+      );
+    });
 };
 
 export const getSavedBiens = () => dispatch => {
   axios
-  .get("/api/estimation/super/biens/saved")
-  .then(res =>
-    dispatch({
-      type : GET_SUPER_SAVED_BIENS,
-      payload : res.data
-    })
-  )
-  .catch(err =>
-    dispatch({
-      type : GET_SUPER_SAVED_BIENS,
-      payload : null
-    })
-  );
+    .get("/api/estimation/super/biens/saved")
+    .then(res =>
+      dispatch({
+        type: GET_SUPER_SAVED_BIENS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_SUPER_SAVED_BIENS,
+        payload: null
+      })
+    );
 };
 
 //deleted saved estimation
 
 export const deleteSavedBien = id => dispatch => {
   axios
-  .delete(`/api/estimation/super/biens/saved/${id}`)
-  .then(res =>
-    dispatch({
-      type: DELETE_SUPER_SAVED_BIEN,
-      payload : id,
+    .delete(`/api/estimation/super/biens/saved/${id}`)
+    .then(res => {
+      dispatch({
+        type: DELETE_SUPER_SAVED_BIEN,
+        payload: id
+      });
+      toast.warn("Votre Estimation a été suprimée avec succès ! ");
     })
-  )
-  .catch(err =>
-    dispatch({
-      type:GET_ERRORS,
-      payload : err.response.data
-    })
-  );
-}
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+      toast.error(
+        "Une Erreur a eu lieu lors de la suppression de cette estimmation : Veuillez réessayer plus tard"
+      );
+    });
+};
